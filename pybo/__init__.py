cbc387b1e1437a82329859e4038e382cfa409ef9
mbc <zxxv7899@gmail.com>
2024-05-24 10:01:52 +0900

    국내 코로나 연력별 통계차트

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,7 +4,12 @@ def create_app():
     app = Flask(__name__)
 
     from .views import main_views
-
     app.register_blueprint(main_views.bp)
+
+    from .views import chart_views
+    app.register_blueprint(chart_views.bp)
+
+    from .views import world_views
+    app.register_blueprint(world_views.bp)
 
     return app
